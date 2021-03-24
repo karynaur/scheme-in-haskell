112ae336485bfdd04ce9db42d766499c5b0367ed
karynaur <adityasrinivas.menon@gmail.com>
2021-03-25 00:55:30 +0530

    lesson 2

--- main.hs
+++ main.hs
@@ -1,10 +1,35 @@
 module Main where
+import Text.ParserCombinators.Parsec hiding (spaces)
 import System.Environment 
 
+data LispVal = Atom String
+             | List [LispVal]
+             | DottedList [LispVal] LispVal
+             | Number Integer
+             | String String
+             | Bool Bool
+
+
+parseString :: Parser LispVal
+parseString = do char '"'
+                 x <- many (noneOf "\"")
+                 char '"'
+                 return $ String x
+
+parseExpr :: Parser LispVal
+parseExpr =  parseString
+
+spaces :: Parser ()
+spaces = skipMany1 space
+
+symbol :: Parser Char
+symbol = oneOf "#$%&|*+-/:<=>?@^_~"
+
+readExpr :: String -> String
+readExpr input = case parse parseExpr "lisp" input of 
+   Left err -> "No match: " ++ show err
+   Right _ -> "Yes" 
+
 main :: IO ()
-main = do args <- getArgs
-          print ((read $ args!!0) + (read $ args!!1))
-   
-          putStrLn ("Write Something :)")
-          line <- getLine
-          putStrLn ("Bruh you just typed " ++ line)
+main = do (expr:_) <- getArgs
+          putStrLn (readExpr expr )
